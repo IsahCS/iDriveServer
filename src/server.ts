c2bd38f60243dff90453c8715c22413d8ff1e954
isabel <isabelcordeiro1230@gmail.com>
2024-11-21 20:23:38 -0300

    register ride confirmation and driver routes in server setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import cors from "@fastify/cors";
 import Fastify from "fastify";
 import { rideEstimate } from "./routes/rideEstimate";
+import { rideConfirm } from "./routes/rideConfirm";
+import { rideDriver } from "./routes/rideDriver";
 
 const app = Fastify({ logger: true });
 
 const start = async () => {
     await app.register(rideEstimate);
+    await app.register(rideConfirm);
+    await app.register(rideDriver);
     try{
         await app.listen({ port: 8080 });
     } catch (err) {
