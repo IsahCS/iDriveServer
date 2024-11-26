db9e75557648b5eb5de7a9f316268272da4ebaac
isabel <isabelcordeiro1230@gmail.com>
2024-11-26 19:01:56 -0300

    Add service to calculate route distance using Google Maps API

---
+++ src/services/serviceCalculateRouteDistance.ts
@@ -0,0 +1,65 @@
+import * as dotenv from 'dotenv';
+import { FastifyReply } from 'fastify';
+import { getAvailableDrivers } from './serviceGetAvailableDrivers';
+
+interface RideRequest {
+    origin: { address: string };
+    destination: { address: string };
+}
+
+dotenv.config();
+const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY || '';
+
+export async function calculateRouteDistance(body: EstimateRequest, reply: FastifyReply) {
+    const { customer_id, origin, destination } = body;
+    const rideRequest: RideRequest = {
+        origin: { address: origin },
+        destination: { address: destination },
+    };
+    const url = 'https://routes.googleapis.com/directions/v2:computeRoutes';
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Goog-Api-Key': googleMapsApiKey,
+                'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.legs.startLocation,routes.legs.endLocation',
+            },
+            body: JSON.stringify(rideRequest),
+        });
+
+        const data = await response.json();
+
+        if (data.routes && data.routes.length > 0) {
+            const route = data.routes[0];
+
+            if (route && route.duration && route.distanceMeters) {
+                const availableDrivers = await getAvailableDrivers(route.distanceMeters);
+                const sortedDrivers = availableDrivers.sort((a, b) => parseFloat(a.value) - parseFloat(b.value));
+
+                return reply.send({
+                    origin: route.legs[0].startLocation.latLng ? {
+                        latitude: route.legs[0].startLocation.latLng.latitude,
+                        longitude: route.legs[0].startLocation.latLng.longitude
+                    } : {},
+                    destination: route.legs[0].endLocation.latLng ? {
+                        latitude: route.legs[0].endLocation.latLng.latitude,
+                        longitude: route.legs[0].endLocation.latLng.longitude
+                    } : {},
+                    distance: route.distanceMeters,
+                    duration: route.duration,
+                    options: sortedDrivers,
+                    routeResponse: data,
+                });
+            };
+        } else {
+            return data;
+        }
+    } catch (error) {
+        return reply.status(500).send({ 
+            error_code: "ROUTE_ERROR",
+            message: "Falha ao calcular a rota." + error,
+        });
+    }
+}
