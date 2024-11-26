9d50b040b729eea6993927fd90fede4b3f317344
isabel <isabelcordeiro1230@gmail.com>
2024-11-26 19:02:32 -0300

    Add validation schema for ride driver request parameters

---
+++ src/services/serviceValidateRideDriver.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+const schema = Joi.object({
+    customer_id: Joi.string().required().messages({
+        "string.empty": "Id do Usuário não pode ser vazio.",
+    })
+});
+
+export async function validateSchemaRideDriver (request: any, reply: any) {
+    try {
+        await schema.validateAsync(request.params, { abortEarly: true });
+    } catch (err) {
+        const error = (err as any).details;
+        return reply.status(400).send({
+            error_code: "INVALID_DATA",
+            error_message: "Os dados fornecidos no corpo da requisição são inválidos.",
+            error: error.map((error: any) => error.message),
+        });
+    }
+};
