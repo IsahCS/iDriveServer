39fdb15d498875b38f01079776a7c0c928127fa0
isabel <isabelcordeiro1230@gmail.com>
2024-11-30 15:32:37 -0300

    Refactor serviceValidateRideConfirm to improve validation messages and enforce correct data types

--- src/services/serviceValidateRideConfirm.ts
+++ src/services/serviceValidateRideConfirm.ts
@@ -12,23 +12,27 @@ const schema = Joi.object({
         "string.empty": "Destino não pode ser vazio.",
     }),
     distance: Joi.number().required().messages({
-        "string.empty": "Distância não pode ser vazio.",
+        "number.base": "Distância deve ser um número.",
+        "number.empty": "Distância não pode ser vazio.",
     }),
     duration: Joi.string().required().messages({
         "string.empty": "Duração não pode ser vazio.",
     }),
     driver: Joi.object({
-        id: Joi.string().required().messages({
-            "string.empty": "ID do motorista não pode ser vazio.",
+        id: Joi.number().required().messages({
+            "number.base": "ID do motorista deve ser um número.",
+            "number.empty": "ID do motorista não pode ser vazio.",
         }),
         name: Joi.string().required().messages({
             "string.empty": "Nome do motorista não pode ser vazio.",
         }),
     }).required().messages({
-        "string.empty": "Motorista não pode ser vazio.",
+        "object.base": "Motorista deve ser um objeto.",
+        "object.empty": "Motorista não pode ser vazio.",
     }),
     value: Joi.number().required().messages({
-        "string.empty": "Valor não pode ser vazio.",
+        "number.base": "Valor deve ser um número.",
+        "number.empty": "Valor não pode ser vazio.",
     })
 });
 
