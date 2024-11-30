8e95a5d72e5440aa1454e2933aad53811d6ad0e5
isabel <isabelcordeiro1230@gmail.com>
2024-11-30 15:31:03 -0300

    Add TypeScript interfaces for Driver, Trip, RideHistory, AvailableDriver, and GetDriver

---
+++ src/services/interface.ts
@@ -0,0 +1,53 @@
+interface Driver {
+    id: number;
+    name: string;
+}
+
+interface Trip {
+    id: number;
+    createdat: Date;
+    origin: string;
+    destination: string;
+    distance: number;
+    duration: string;
+    driverid: number;
+    drivername: string;
+    value: number;
+}
+
+interface RideHistory {
+    customer_id: string;
+    rides: {
+        id: number;
+        date: Date;
+        origin: string;
+        destination: string;
+        distance: number;
+        duration: string;
+        driver: Driver;
+        value: number;
+    }[];
+}
+
+interface AvailableDriver {
+    id: number;
+    name: string;
+    description: string;
+    vehicle: string;
+    review: {
+        rating: number;
+        comment: string;
+    };
+    value: string;
+}
+
+interface GetDriver {
+    id: number;
+    name: string;
+    description: string;
+    car: string;
+    rating: number;
+    review: string;
+    minKm: number;
+    ratePerKm: number;
+}
